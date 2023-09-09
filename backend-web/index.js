2f41665242ab8ab23baf80252f43693cf4d1e76b
B1909890 <duyb1909890@student.ctu.edu.vn>
2023-09-09 11:38:16 +0700

    update

--- backend-web/index.js
+++ backend-web/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
+app.get('/tc1', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
   console.log(`Server đang chạy`)
+});
+
+app.get('/c1', (req, res) => {
+  res.send('Hello World!');
 });
